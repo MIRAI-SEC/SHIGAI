c9d6b7c91ad2eba28da63faa46595c0d9b1e6f7d
Denailly <danilocalheirosjr@gmail.com>
2023-08-06 01:47:54 -0300

    First Commit!

---
+++ SHIGAI/target/debug/deps/SHIGAI-bd595003a15ea6f8.d
@@ -0,0 +1,5 @@
+C:\Users\danil\Desktop\SHIGAI\SHIGAI\target\debug\deps\SHIGAI-bd595003a15ea6f8.rmeta: src\main.rs
+
+C:\Users\danil\Desktop\SHIGAI\SHIGAI\target\debug\deps\SHIGAI-bd595003a15ea6f8.d: src\main.rs
+
+src\main.rs:
---
+++ SHIGAI/target/debug/deps/SHIGAI-d7dec79385518fde.d
@@ -0,0 +1,5 @@
+C:\Users\danil\Desktop\SHIGAI\SHIGAI\target\debug\deps\SHIGAI-d7dec79385518fde.rmeta: src\main.rs
+
+C:\Users\danil\Desktop\SHIGAI\SHIGAI\target\debug\deps\SHIGAI-d7dec79385518fde.d: src\main.rs
+
+src\main.rs:
